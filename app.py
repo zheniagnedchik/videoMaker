6ee5160e02a0017f5b1e634efb58b93c39465d0f
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2024-04-19 05:10:32 -0700

    add

--- app.py
+++ app.py
@@ -37,15 +37,15 @@ def process_video(video_path, image_url, output_path):
     frame_height = int(cap.get(4))
 
     videos_dir = '/var/www/videoMaker/videos/'
-    # videos_dir = 'videos/'
+    videos_dir = 'videos/'
     if not os.path.exists(videos_dir):
         os.makedirs(videos_dir)
 
     full_output_path = os.path.join(videos_dir, output_path)
     # out = cv2.VideoWriter(full_output_path.replace(
     #     '.mp4', '.webm'), cv2.VideoWriter_fourcc(*'VP80'), 20.0, (frame_width, frame_height))
-    out = cv2.VideoWriter(full_output_path.replace('.mp4', '.mp4'),  # Changed to MP4
-                          cv2.VideoWriter_fourcc(*'avc1'),  # Use H.264 codec
+    out = cv2.VideoWriter(full_output_path,
+                          cv2.VideoWriter_fourcc(*'XVID'),  # Use 'XVID' codec
                           20.0, (frame_width, frame_height))
 
     insert_image = download_image(image_url)
@@ -70,28 +70,35 @@ def process_video(video_path, image_url, output_path):
 
     cap.release()
     out.release()
-    mp4_output_path = output_path.replace('.webm', '.mp4')
-    # full_mp4_output_path = os.path.join(videos_dir, mp4_output_path)
-    # convert_to_mp4(full_output_path, full_mp4_output_path)
+    mp4_output_path = output_path.replace('.mp4', '.mp4')
+    full_mp4_output_path = os.path.join(videos_dir, mp4_output_path)
+    convert_to_mp4(full_output_path, full_mp4_output_path)
 
-    return mp4_output_path
+    return full_mp4_output_path
     # return full_output_path
 
 
 @app.route('/videos/<path:filename>')
 def get_video(filename):
     videos_dir = '/var/www/videoMaker/videos/'
-    # videos_dir = 'videos/'
+    videos_dir = 'videos/'
     full_path = os.path.join(videos_dir, filename)
     return send_file(full_path, mimetype='video/mp4')
 
 
 def convert_to_mp4(input_path, output_path):
-    # Replace the placeholder below with the actual path to FFmpeg as found with `which ffmpeg` or `where ffmpeg`
+    # Check if input and output paths are the same
+    if input_path == output_path:
+        base, ext = os.path.splitext(output_path)
+        output_path = f"{base}_converted{ext}"
+
+    # Replace the placeholder below with the actual path to FFmpeg
     ffmpeg_path = '/opt/homebrew/bin/ffmpeg'  # Example path on Linux
     command = [ffmpeg_path, '-i', input_path,
                '-vcodec', 'libx264', '-crf', '23', output_path]
     subprocess.run(command, check=True)
+
+    return output_path
 
 
 @app.route('/process', methods=['POST'])
